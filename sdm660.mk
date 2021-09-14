8b6539ac727ed0b88c6d5a87b57a5a08f490e430
SayanAcharya <SayanAcharya@users.noreply.github.com>
2021-09-14 13:38:08 +0000

    sdm660-common: Import Capabiity Configstore Blobs
    - From LA.UM.9.12.r1-10800-SMxx50
    - Required After fresh Manifest Import
    
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -109,10 +109,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0
 
-# Cgroup and task_profiles
-PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+# Capability Configstore
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
