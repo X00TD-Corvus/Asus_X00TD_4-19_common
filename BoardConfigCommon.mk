5d38f07e2817f987bc8b9ad6414bc8709f0d3665
Sonal Singh <sonal.singh.19993@gmail.com>
2022-12-22 14:49:58 +0530

    sdm660-common: Switch to cosmic clang conditionally
     - as arrow jenkins builds are not compilable with custom clang
       due to outdated glibc version
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,8 +80,13 @@ BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
 
 TARGET_KERNEL_SOURCE := kernel/asus/sdm660
-TARGET_KERNEL_VERSION := 4.19
-#TARGET_KERNEL_CLANG_VERSION := proton
+
+ifeq ($(CUSTOM_CLANG),true)
+TARGET_KERNEL_CLANG_VERSION := cosmic
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-cosmic
+TARGET_KERNEL_ADDITIONAL_FLAGS := AR=llvm-ar AS=llvm-as NM=llvm-nm LD=ld.lld OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump OBJSIZE=llvm-size READELF=llvm-readelf STRIP=llvm-strip HOSTAR=llvm-ar HOSTAS=llvm-as HOSTNM=llvm-nm HOSTLD=ld.lld
+endif
+
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
